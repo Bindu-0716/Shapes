e63f0441187ef561a86b163ccadbf1215c5ef72b
Bindu-1607 <bindu07.b@gmail.com>
2021-05-04 12:58:48 +0530

    Initial commit

---
+++ src/com/java/shapes/main.groovy
@@ -0,0 +1,15 @@
+package com.java.shapes
+class Overload
+{
+     static void main(String[] args)
+    {
+        OverloadDemo ob = new OverloadDemo()
+        ob.area(5)
+        ob.area(11,12)
+        ob.area(2.5)
+    }
+}
+
+
+
+
